6a5e37833ddd2120e3f0d7f0e779c5198a6e0218
m305-62\w401 <aierzhiguang2@gmail.com>
2018-03-22 16:54:38 +0800

    可以显示01，02，等 可以一次显示10个

--- LABA/Program.cs
+++ LABA/Program.cs
@@ -10,18 +10,20 @@ namespace LABA
     {
         static void Main(string[] args)
         {
+            int n= 1;
             Random Rand = new Random();
-            int[] r = new int[6];
-            int r1 = Rand.Next(1, 43);
-            int r2 = Rand.Next(1, 43);
-            int r3 = Rand.Next(1, 43);
-            int r4 = Rand.Next(1, 43);
-            int r5 = Rand.Next(1, 43);
-            int r6 = Rand.Next(1, 43);
-            int r7 = Rand.Next(1, 43);
+            while (n <= 10)
+            {
+                int r1 = Rand.Next(1, 43);
+                int r2 = Rand.Next(1, 43);
+                int r3 = Rand.Next(1, 43);
+                int r4 = Rand.Next(1, 43);
+                int r5 = Rand.Next(1, 43);
+                int r6 = Rand.Next(1, 43);
 
-            Console.WriteLine("number:{0:00} {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}", r1, r2, r3, r4, r5, r6,r7);
-
+                Console.WriteLine("number:[{0:00}] {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}",n, r1, r2, r3, r4, r5, r6);
+                n += 1;
+            }
             Console.ReadLine();
         }
     }
