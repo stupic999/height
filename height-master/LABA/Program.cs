86a78619b3d5f97b0dbc3e380979d7723e1b5f68
stupic999 <37150488+stupic999@users.noreply.github.com>
2018-03-22 23:37:09 +0800

    完成陣列，註解，數字不會重複，按一個鍵就可以推出

---
+++ height-master/LABA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n= 1;
+            Random Rand = new Random();
+            int[] r = new int[6];
+            while (n <= 10)
+            {
+                for (int i = 0; i < 6; i++)
+                r[i] = Rand.Next(1, 43);
+                Console.WriteLine("number:[{0:00}] {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}",n, r[0], r[1], r[2], r[3], r[4], r[5]);
+                n += 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
