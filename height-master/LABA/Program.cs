13cb9d3d3955db5d6b9ea7e2ec2ad3019ac542c4
stupic999 <37150488+stupic999@users.noreply.github.com>
2018-03-26 12:30:54 +0800

    寫好刷新，會隨機跳動五十次才最終決定數字，然後在學校電腦上傳之後自己的電腦不知道為什麼不可以通過source tree 上傳，只好直接傳到網頁

--- height-master/LABA/Program.cs
+++ height-master/LABA/Program.cs
@@ -13,13 +13,18 @@ namespace LABA
             int n= 1;
             Random Rand = new Random();
             int[] r = new int[6];
-            while (n <= 10)
+            while (n <50)
             {
                 for (int i = 0; i < 6; i++)
-                r[i] = Rand.Next(1, 43);
-                Console.WriteLine("number:[{0:00}] {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}",n, r[0], r[1], r[2], r[3], r[4], r[5]);
+                {
+                    r[i] = Rand.Next(1, 43);
+                }
+                System.Threading.Thread.Sleep(50);
+                Console.Clear();
+                Console.WriteLine("number: {0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", r[0], r[1], r[2], r[3], r[4], r[5]);
                 n += 1;
             }
+            Console.WriteLine("today prices is: {0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", r[0], r[1], r[2], r[3], r[4], r[5]+", good luck ^^");
             Console.ReadLine();
         }
     }
