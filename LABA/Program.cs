049644730d7c1fde30a1f8ade4db8525a8c37095
stupic999 <37150488+stupic999@users.noreply.github.com>
2018-04-22 19:54:53 +0800

    刪減了無用的文件和註解

--- LABA/Program.cs
+++ LABA/Program.cs
@@ -10,10 +10,14 @@ namespace LABA
     {
         static void Main(string[] args)
         {
+            // 設定迴圈執行的次數
             int n= 1;
+
             Random Rand = new Random();
+
             while (n <= 10)
             {
+                // 設定r1～r6，隨機從1到42選取一個數帶入
                 int r1 = Rand.Next(1, 43);
                 int r2 = Rand.Next(1, 43);
                 int r3 = Rand.Next(1, 43);
@@ -22,9 +26,12 @@ namespace LABA
                 int r6 = Rand.Next(1, 43);
 
                 Console.WriteLine("number:[{0:00}] {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}",n, r1, r2, r3, r4, r5, r6);
+                
                 n += 1;
             }
-            Console.ReadLine();
+
+            // 按任何一個按鈕即可退出
+            Console.ReadKey();
         }
     }
 }
